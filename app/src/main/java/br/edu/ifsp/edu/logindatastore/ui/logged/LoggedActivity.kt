1fc04fcdfd32a9991751f97720fc81c4bb35b04e
deivid <deivid9pereira@gmail.com>
2025-01-06 11:08:16 -0300

    camada de UI

--- app/src/main/java/br/edu/ifsp/edu/logindatastore/ui/logged/LoggedActivity.kt
+++ app/src/main/java/br/edu/ifsp/edu/logindatastore/ui/logged/LoggedActivity.kt
@@ -1,4 +1,21 @@
 package br.edu.ifsp.edu.logindatastore.ui.logged
 
-class LoggedActivity {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import br.edu.ifsp.edu.logindatastore.databinding.ActivityLoggedBinding
+
+class LoggedActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoggedBinding
+    private lateinit var viewModel: LoggedViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoggedBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        viewModel = ViewModelProvider(this).get(LoggedViewModel::class.java)
+        binding.textMessage.setText("Bem-Vindo")
+    }
+
 }
---
+++ app/src/main/java/br/edu/ifsp/edu/logindatastore/ui/logged/LoggedViewModel.kt
@@ -0,0 +1,6 @@
+package br.edu.ifsp.edu.logindatastore.ui.logged
+
+import androidx.lifecycle.ViewModel
+
+class LoggedViewModel : ViewModel() {
+}
